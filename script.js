41ff3bbd87137f7751f4e4cf3d766a8a335a62f7
Carson Hartley <chartley1988@gmail.com>
2022-07-27 16:28:55 -0600

    Hopefully touch works now.

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
-const displayValue = document.getElementsByClassName('display')[0];
-const interface = document.getElementsByClassName('interface')[0];
-let buttons = Array.from(interface.children);
-buttons.shift(); // Removes the answer display from button list
 
-buttons.forEach(button => {
-    button.addEventListener('click', event => {
-        console.log(event.target.textContent);
-        reset(event.target.textContent);
+connectButtons();
+
+function getButtons() {
+    const interface = document.getElementsByClassName('interface')[0];
+    let buttons = Array.from(interface.children);
+    buttons.shift();
+    return buttons; // Removes the answer display from button list
+}
+
+function connectButtons() {
+    let buttons = getButtons();
+    buttons.forEach(button => {
+        button.addEventListener('click', updateDisplay)
+        button.addEventListener('touchend', updateDisplay)
     })
-})
+}
    
-
-
-
 function operate(operator, num1, num2) { 
-    console.log("Temp")
+    switch(operator){
+        case "+": 
+            add(num1, num2);
+        case "-": 
+            subtract(num1, num2);
+        case "*": 
+            multiply(num1, num2);
+        case "/": 
+            divide(num1, num2);
+        default: 
+            return;
+    }
 }
 
-function reset(text) {
-    displayValue.textContent = text;
+function updateDisplay(text) {
+    const displayValue = document.getElementsByClassName('display')[0];
+    displayValue.textContent = text.target.textContent;
 }
 
-function add() {
-    //
+function add(num1, num2) {
+    return(num1 + num2);
 }
 
-function subtract() {
-    //
+function subtract(num1, num2) {
+    return(num1 - num2);
 }
 
-function multiply() {
-    //
+function multiply(num1, num2) {
+    return(num1 - num2);
 }
 
-function divide() {
-
+function divide(num1, num2) {
+    return(num1 - num2);
 }
